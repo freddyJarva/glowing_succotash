d13c7acb688b642a9b72be5bf90aebe34ec98b68
Freddy Järvå <freddy.jarva@gmail.com>
2020-03-14 12:27:23 +0100

    backend - create character model

--- backend/character/models.py
+++ backend/character/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Character(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=100)
+    peng = models.IntegerField(default=0)
+    food = models.IntegerField(default=0)
+    water = models.IntegerField(default=0)
+
+    # Grundegenskaper
+    strength = models.IntegerField()
+    intelligence = models.IntegerField()
+    agility = models.IntegerField()
+
+    # Values taken from other tables, just mock data for now
+    profession = models.TextField()
+    abilities = models.TextField()
+    specials = models.TextField()
+
+    armors = models.TextField(null=True)
+    weapons = models.TextField(null=True)
+    items = models.TextField(null=True)
